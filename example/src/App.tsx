501e0888b45d15afb42a2adbc07aca96e8d15bc7
Krzysztof Bonecki <30378759+chrizuuu@users.noreply.github.com>
2023-05-03 15:20:37 +0200

    feat: add example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,52 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-image-viewer';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import ImageViewer from '../../src';
+
+const images = [
+  {
+    uri: 'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1501183007986-d0d080b147f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1536782376847-5c9d14d97cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1506038634487-60a69ae4b7b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1538991383142-36c4edeaffde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1430990480609-2bf7c02a6b1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1509514026798-53d40bf1aa09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+];
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [isVisible, setisVisible] = React.useState(false);
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+  const handleOpenImageViewer = React.useCallback(() => {
+    setisVisible(true);
+  }, []);
+
+  const handleCloseImageViewer = React.useCallback(() => {
+    setisVisible(false);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={styles.text}>react-native-image-viewer</Text>
+      <Button title="Open image viewer" onPress={handleOpenImageViewer} />
+      <ImageViewer
+        isVisible={isVisible}
+        onRequestClose={handleCloseImageViewer}
+        images={images}
+      />
     </View>
   );
 }
@@ -27,5 +61,9 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '700',
   },
 });
